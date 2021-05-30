9c120beaa5d08d78d035466a8ebf1831f2e90393
Tannar <tannar.james@gmail.com>
2021-05-29 20:18:40 -0500

    Finished

--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -130,7 +130,8 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
